2f70d3d69306ac6ac477cf11b94e7542475d2d1a
smschijf <210578@student.glu.nl>
2022-04-20 10:21:34 +0200

    basic layout done

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/input.css">
   <title>Document</title>
 </head>
+
 <body>
-  <h1 class="text-3xl underline">Hello World</h1>
+  <div class="container">
+    <section>
+      <div class="header-container">
+        <img src="assets/img/logo.png" alt="logo">
+        <h1 class="text-3xl">VrijwilligersHuis Nieuwegein</h1>
+      </div>
+      <div class="info-container">
+        <h2 class="text-xl">Our misson</h2>
+      </div>
+    </section>
+    <div class="form-container flex">
+      <form>
+        form
+      </form>
+    </div>
+  </div>
 </body>
+
 </html>
