451806dcb08e3b0bc035b9afcd0aaac3f991f0ac
CcharlotteHh <210547@student.glu.nl>
2022-04-21 10:28:43 +0200

    changes

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <title>Document</title>
-</head>
+<?php
+include 'connect.php';
+include 'header.php';
+include 'process.php';
+?>
 
 <body>
   <div class="split-screen">
@@ -18,7 +13,7 @@
       </section>
     </div>
     <div class="right">
-      <form action="process.php" method="POST">
+      <form action="process.php" method="post">
         <section class="copy">
           <h2>Formulier</h2>
 
@@ -26,18 +21,18 @@
         <div class="input-container name">
           <label for="fname">Volledige naam</label>
           <div class="name-input-container">
-          <input class="name-input" id="fname" name="voornaam" type="text" placeholder="Voornaam" required>
-          <input class="tv-input"  id="fname" name="tussenvoegsel" type="text" placeholder="tvs">
-          <input class="name-input" id="fname" name="achternaam" type="text" placeholder="Achterrnaam" required>
+            <input class="name-input" name="voornaam" type="text" placeholder="Voornaam" required>
+            <input class="tv-input" name="tussenvoegsel" type="text" placeholder="tvs">
+            <input class="name-input" name="achternaam" type="text" placeholder="Achterrnaam" required>
           </div>
         </div>
         <div class="input-container email">
           <label for="email">E-mail</label>
-          <input id="email" name="email" type="text" placeholder="E-mail" required>
+          <input name="email" type="text" placeholder="E-mail" required>
         </div>
         <div class="input-container phone">
           <label for="tel">Telefoonnummer</label>
-          <input id="tel" name="tel_nummer" type="text" placeholder="Telefoonnummer">
+          <input name="tel_nummer" type="text" placeholder="Telefoonnummer">
         </div>
         <div class="input-container">
           <label for="message">Vertel ons wat over hoe je wil helpen</label>
@@ -45,18 +40,22 @@
         </div>
         <div class="input-container">
           <label for="category">Selecteer category</label>
-          <select name="category-product"required>
-            <option value="">Selecteer een category</option>
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="hamster">Hamster</option>
-            <option value="parrot">Parrot</option>
-            <option value="spider">Spider</option>
-            <option value="goldfish">Goldfish</option>
-            </select>
-            </div>
-            <button type="submit" class="signup-btn" name="submit">Verstuur</button>
-        
+          <select name="category-product" required>
+            <?php
+            $sql = "select category_id, name from category";
+            $result = mysqli_query($conn, $sql);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+              <option value="<?php echo $row['category_id'] ?>"><?php echo $row['name'] ?></option>
+
+
+            <?php
+            } ?>
+          </select>
+        </div>
+        <button type="submit" class="signup-btn" name="submit">Verstuur</button>
+
       </form>
     </div>
   </div>
